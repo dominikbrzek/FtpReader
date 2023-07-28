dce678eb08270d2d65176516ea7282bff20f0127
dobr_kipertech <dobr@kipertech.com>
2023-07-28 09:04:24 +0200

    initial commit

---
+++ src/main/java/com/example/ftpreader/FtpReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.ftpreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtpReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ftpreader/FtpReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ftpreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
